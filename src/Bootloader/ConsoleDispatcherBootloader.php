903791277cbda0b518148d3946bb461b448bb695
ncou <nicolas_courade@yahoo.fr>
2020-09-19 14:18:36 +0200

    update

---
+++ src/Bootloader/ConsoleDispatcherBootloader.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Chiron\Console\Bootloader;
+
+use Chiron\Application;
+use Chiron\Bootload\AbstractBootloader;
+use Chiron\Config\AppConfig;
+use Chiron\Console\ConsoleDispatcher;
+
+final class ConsoleDispatcherBootloader extends AbstractBootloader
+{
+    public function boot(Application $application, AppConfig $config): void
+    {
+        $application->addDispatcher(resolve(ConsoleDispatcher::class));
+    }
+}
